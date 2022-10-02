c3250c3d067335fb7ce8bdd909219cd67669b3ce
Umang Patel <umangpatel3105@gmail.com>
2022-10-02 13:32:44 +0530

    fea: Added compose

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import { useState } from 'preact/hooks'
+import Compose from '../components/Compose'
 export default function Dashboard() {
+  const [modal,setModal]=useState(false)
   return (
     <div>
-        <Sidebar />
+        <Sidebar modal={modal} setModal={setModal} />
+        {modal && <Compose setModal={setModal}/> }
     </div>
   )
 }
