287bb146062cc07e0dbb90e753c023c00ec9bf60
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-10-02 16:09:49 +0530

    fea:done

--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -5,7 +5,7 @@ export default function MailBox({ mails }) {
   return (
     <div className="bg-slate-500 flex-1 h-screen overflow-y-scroll overflow-x-hidden ">
       <div className="text-4xl text-white p-6">All Mails</div>
-      <hr className="border-t-2 border-black mx-4"/>
+      <hr className="border-t-2 border-black mx-4" />
       {mails &&
         mails.map((mail, i) => {
           var date = convertDate(mail.timeStamp);
@@ -13,13 +13,12 @@ export default function MailBox({ mails }) {
           console.log(date);
           return (
             <a href={`/mailID/${i}`}>
-              <div className="bg-slate-300 w-screen my-2 p-4 mx-2 rounded-sm">
+              <div className="bg-slate-300 w-screen my-4 p-4 mx-2 rounded-sm hover:scale-95 transition-all shadow-md">
                 <span>From : {mail.sender}</span>
                 <h1>Subject: {mail.subject}</h1>
                 <br />
                 <span>At {date}</span>
                 <br />
-                <span>{mail.body}</span>
               </div>
             </a>
           );
